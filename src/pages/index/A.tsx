df5e044b0903488d195c2c1c00fbc2a29b8677b2
Sociosarbis <136657577@qq.com>
2021-07-04 16:10:49 +0800

    feat: :tada: init

---
+++ src/pages/index/A.tsx
@@ -0,0 +1,12 @@
+import { View } from '@tarojs/components'
+import { defineComponent, h } from 'vue'
+
+
+
+const A = defineComponent({
+  setup() {
+    return () => <View>3</View>
+  }
+})
+
+export default A
---
+++ src/pages/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <view class="index">
+    <text>{{ msg }}</text>
+    <A />
+  </view>
+</template>
+
+<script>
+import { ref } from 'vue'
+import './index.css'
+import A from './A'
+
+export default {
+  components: {
+    A
+  },
+  setup () {
+    const msg = ref('Hello world')
+    return {
+      msg
+    }
+  }
+}
+</script>
